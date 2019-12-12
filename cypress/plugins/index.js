20a1c2c95333a92950afef954a1d75a7b2465c31
RoyalSix <jay.a.scottjr@biola.edu>
2019-12-12 08:46:35 -0800

    nyc output is working

---
+++ cypress/plugins/index.js
@@ -0,0 +1,5 @@
+module.exports = (on, config) => {
+  on('file:preprocessor', require('@cypress/code-coverage/use-browserify-istanbul'))
+  on('task', require('@cypress/code-coverage/task'))
+  return config;
+}
