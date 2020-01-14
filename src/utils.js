b79b3b5b2d0824ebdde5b3aa71755f5937ae0590
RoyalSix <jay.a.scottjr@biola.edu>
2020-01-14 11:51:47 -0800

    disabled back button different implementation

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,6 @@
 export function disableBackButton() {
-  window.addEventListener('load', function () {
-    window.history.pushState(null, document.title, window.location.href);
-    window.addEventListener('popstate', function () {
-      window.history.pushState(null, document.title, window.location.href);
-    });
-  })
+  window.history.pushState(null, null, window.location.href);
+  window.history.back();
+  window.history.forward();
+  window.onpopstate = function () { window.history.go(1); };
 }
