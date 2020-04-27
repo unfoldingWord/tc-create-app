dad1a9120bb04d5786d4e97ea81979aa78014eb6
RoyalSix <jay.a.scottjr@biola.edu>
2020-04-27 10:14:35 -0700

    added fix

--- src/core/state.defaults.js
+++ src/core/state.defaults.js
@@ -1,19 +1,18 @@
 import appPackage from '../../package.json';
 
 export const SERVER_URL = process.env.REACT_APP_DOOR43_SERVER_URL;
-const REPO_URL = process.env.REACT_APP_DOOR43_REPO_URL;
 
 const config = {
   authentication: {
-    server: REPO_URL,
+    server: SERVER_URL,
     tokenid: appPackage.name,
   },
   repository: {
     urls: [
-      REPO_URL + '/api/v1/repos/unfoldingword/en_ta',
-      REPO_URL + '/api/v1/repos/unfoldingword/en_tw',
-      REPO_URL + '/api/v1/repos/unfoldingword/en_tn',
-      REPO_URL + '/api/v1/repos/unfoldingword/en_obs',
+      SERVER_URL + '/api/v1/repos/unfoldingword/en_ta',
+      SERVER_URL + '/api/v1/repos/unfoldingword/en_tw',
+      SERVER_URL + '/api/v1/repos/unfoldingword/en_tn',
+      SERVER_URL + '/api/v1/repos/unfoldingword/en_obs',
     ],
   },
 };
