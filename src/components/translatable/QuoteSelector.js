80b73744ac7421689cf53e1f5039e2e10ce33b23
mandolyte <cecil.new@gmail.com>
2020-05-22 12:01:40 -0400

    WIP hooks and context into tCC

--- src/components/translatable/QuoteSelector.js
+++ src/components/translatable/QuoteSelector.js
@@ -1,11 +1,13 @@
 import React, { useMemo } from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import { ParallelScripture, withResources } from 'scripture-resources-rcl';
+//import { ParallelScripture, withResources } from 'scripture-resources-rcl';
+import {ParallelScripture, ResourcesContextProvider} 
+  from "scripture-resources-rcl";
 import { testament } from '../../core/bcv.js';
 import { SERVER_URL } from '../../core/state.defaults';
 import { getMuiTheme } from './muiTheme';
 
-const ParallelScriptureWithResources = withResources(ParallelScripture);
+//const ParallelScriptureWithResources = withResources(ParallelScripture);
 
 function QuoteSelector({
   quote,
@@ -20,27 +22,54 @@ function QuoteSelector({
   let greekLink = 'unfoldingWord/el-x-koine/ugnt/master';
   let originalLink = (_testament === 'old') ? hebrewLink : greekLink;
 
-  const resourceLinks = [
+  // need to add reference bookId to resource links
+  const _resourceLinks = [
     originalLink,
     'unfoldingWord/en/ult/master',
     'unfoldingWord/en/ust/master',
   ];
+  const bookId = reference.bookId;
+  const resourceLinks = _resourceLinks.map( (link) => {
+    return link+'/'+bookId;
+  });
   const occurrence = (_occurrence == "\\-1") ? -1 : _occurrence;
+
+  // manage the state of the resources for the provider context
+  const [ resources, setResources ] = React.useState([]);
 
   return (
     <MuiThemeProvider theme={getMuiTheme}>
-      <ParallelScriptureWithResources
+      <ResourcesContextProvider
         resourceLinks={resourceLinks}
+        resources={resources}
+        onResources={setResources}
         config={config}
-        reference={reference}
-        quote={quote}
-        onQuote={onQuote}
-        occurrence={occurrence}
-        height='250px'
-        buttons={buttons}
-      />
+      >
+        <ParallelScripture
+          resources={resources}
+          reference={reference}
+          quote={quote}
+          onQuote={onQuote}
+          occurrence={occurrence}
+          height='250px'
+          buttons={buttons}
+        />
+      </ResourcesContextProvider>
     </MuiThemeProvider>
   );
 };
 
 export default QuoteSelector;
+
+/*
+        <ParallelScriptureWithResources
+          resourceLinks={resourceLinks}
+          config={config}
+          reference={reference}
+          quote={quote}
+          onQuote={onQuote}
+          occurrence={occurrence}
+          height='250px'
+          buttons={buttons}
+        />
+*/
