bba4d9dbcdf676c3237a0a6ee02484e6eb64d4e6
RoyalSix <jay.a.scottjr@biola.edu>
2020-07-28 09:58:59 -0700

    added fix for renderHook gotchas

--- __tests__/useStateReducer.spec.js
+++ __tests__/useStateReducer.spec.js
@@ -2,13 +2,27 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import { useStateReducer } from '../src/core/useStateReducer';
 import { defaultState } from './fixtures/useStateReducer';
 
-
-//TODO:
 describe('useStateReducer', () => {
-  test('', () => {
+  test('should use default state and match snapshots of objects', () => {
     const { result } = renderHook(() => useStateReducer(defaultState));
     const [state, actions] = result.current;
     expect(state).toMatchSnapshot();
     expect(actions).toMatchSnapshot();
+  });
+
+  test('should test setTargetRepoFromSourceRepo', () => {
+    const { result } = renderHook(() => useStateReducer(defaultState));
+    const expectedOutput = 'https://new-url.com';
+
+    act(() => {
+      result.current[1].setConfig({
+        ...result.current[0].config,
+        authentication: {
+          ...result.current[0].config.authentication,
+          server:expectedOutput,
+        },
+      });
+    });
+    expect(result.current[0].config.authentication.server).toBe(expectedOutput);
   });
 });
