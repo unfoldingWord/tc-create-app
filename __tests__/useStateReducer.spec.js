acd572c9dc4766a938b2a006bee96b0e86dd4642
RoyalSix <jay.a.scottjr@biola.edu>
2020-07-28 10:02:41 -0700

    added another state reducer test

--- __tests__/useStateReducer.spec.js
+++ __tests__/useStateReducer.spec.js
@@ -1,6 +1,7 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import { useStateReducer } from '../src/core/useStateReducer';
 import { defaultState } from './fixtures/useStateReducer';
+jest.mock('localforage');
 
 describe('useStateReducer', () => {
   test('should use default state and match snapshots of objects', () => {
@@ -24,5 +25,14 @@ describe('useStateReducer', () => {
       });
     });
     expect(result.current[0].config.authentication.server).toBe(expectedOutput);
+  });
+
+  test('should set empty source repository', () => {
+    const { result } = renderHook(() => useStateReducer(defaultState));
+
+    act(() => {
+      result.current[1].setLanguage();
+    });
+    expect(result.current[0].sourceRepository).toBeFalsy();
   });
 });
