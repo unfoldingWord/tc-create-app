e88e55bd2d6ad9b14ffc47ad853e6767af213204
RoyalSix <jay.a.scottjr@biola.edu>
2020-08-05 14:38:41 -0700

    fixed cypress tests

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,5 @@
 module.exports = (on, config) => {
-  on('file:preprocessor', require('@cypress/code-coverage/use-browserify-istanbul'))
-  on('task', require('@cypress/code-coverage/task'))
+  require('@cypress/code-coverage/task')(on, config);
+  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
   return config;
-}
+};
