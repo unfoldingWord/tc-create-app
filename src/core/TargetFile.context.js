135147f85963f045557e173a3d7a34210406e35f
ancientTexts-net <zach.pennington@unfoldingWord.org>
2020-12-01 10:47:53 -0600

    Review (removed ValidateOnOpen)

--- src/core/TargetFile.context.js
+++ src/core/TargetFile.context.js
@@ -3,20 +3,19 @@ import PropTypes from 'prop-types';
 import { useFile, FileContext } from 'gitea-react-toolkit';
 import useEffect from 'use-deep-compare-effect';
 import { AppContext } from '../App.context';
-//import ValidateOnOpenDialog from './ValidateOnOpenDialog';
 import * as cv from 'uw-content-validation';
 
 const TargetFileContext = React.createContext();
 
-function TargetFileContextProvider({ 
-  validated, onValidated, onCriticalErrors, children 
+function TargetFileContextProvider({
+  validated, onValidated, onCriticalErrors, children
 }) {
   const {
     state: {
       authentication, targetRepository, filepath, setFilepath,
-    }={},
+    } = {},
   } = useContext(AppContext);
-  
+
   const { state: sourceFile } = useContext(FileContext);
 
   const {
@@ -32,60 +31,60 @@ function TargetFileContextProvider({
 
   const validate = async (langId, bookID, content) => {
     return await cv.checkTN_TSVText(langId, bookID, 'dummy', content, '',
-      {checkLinkedTAArticleFlag: false, checkLinkedTWArticleFlag: false}
+      { checkLinkedTAArticleFlag: false, checkLinkedTWArticleFlag: false }
     );
   }
 
-  useEffect( () => {
-    if ( state === undefined || state?.content === undefined ) {
+  useEffect(() => {
+    if (state === undefined || state?.content === undefined) {
       onValidated(false);
       onCriticalErrors(['Validating...']);
-    } else if ( !validated ) {
-      const link = state.html_url.replace('/src/','/blame/');
+    } else if (!validated) {
+      const link = state.html_url.replace('/src/', '/blame/');
       let criticalNotices = [];
       let tsvFile = state.content.trimEnd();
       // Split into an array of rows
       let rows = tsvFile.split('\n');
       // Is the first row correct (must have the correct headers)
       let tsvHeader = "Book\tChapter\tVerse\tID\tSupportReference\tOrigQuote\tOccurrence\tGLQuote\tOccurrenceNote";
-      if ( tsvHeader !== rows[0] ) {
+      if (tsvHeader !== rows[0]) {
         criticalNotices.push([
           `${link}#L1`,
-          '1', 
-          `Bad TSV Header, expecting "${tsvHeader.replaceAll('\t',',')}"`]);
+          '1',
+          `Bad TSV Header, expecting "${tsvHeader.replaceAll('\t', ',')}"`]);
       }
 
-      if ( rows.length > 1 ) {
-        for ( let i=1; i < rows.length; i++ ) {
-          let line = i+1;
+      if (rows.length > 1) {
+        for (let i = 1; i < rows.length; i++) {
+          let line = i + 1;
           let cols = rows[i].split('\t');
-          if ( cols.length < 9 ) {
+          if (cols.length < 9) {
             criticalNotices.push([
               `${link}#L${line}`,
-              `${line}`, 
+              `${line}`,
               `Not enough columns, expecting 9, found ${cols.length}`
             ])
-          } else if ( cols.length > 9 ) {
+          } else if (cols.length > 9) {
             criticalNotices.push([
               `${link}#L${line}`,
-              `${line}`, 
+              `${line}`,
               `Too many columns, expecting 9, found ${cols.length}`
             ])
           }
         }
       }
-      
-      if ( criticalNotices.length > 0 ) {
+
+      if (criticalNotices.length > 0) {
         onCriticalErrors(criticalNotices);
       } else {
         onValidated(true);
       }
     }
-  }, [validated, onValidated, state, onCriticalErrors]);   
+  }, [validated, onValidated, state, onCriticalErrors]);
 
   const context = {
-    state: {...state, validated}, // state true/false
-    actions: {...actions, validate}, // add my action
+    state: { ...state, validated }, // state true/false
+    actions: { ...actions, validate }, // add my action
     component,
     components,
     config,
