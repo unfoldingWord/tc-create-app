6d5f98d82240ce85c26a30fff8e3c6902d7f7d03
RoyalSix <jay.a.scottjr@biola.edu>
2020-12-28 15:33:02 -0800

    adding badge script back to ci

--- scripts/create-badge-json.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const coveageString = fs.readFileSync('coverage/coverage-summary.json', 'utf8');
-const coverage = JSON.parse(coveageString);
-const total = coverage.total.lines.pct;
-const output = {
-  'message':`${total}%`,'label':'Integration Tests','schemaVersion':1,
-};
-
-fs.writeFileSync('coverage/shields.json', JSON.stringify(output, null, 2));
