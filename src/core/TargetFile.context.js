c17ef37cd3e1cd2c143f3316f6942cb47886a4b4
mandolyte <cecil.new@gmail.com>
2021-04-29 10:43:57 -0400

    clean point

--- src/core/TargetFile.context.js
+++ src/core/TargetFile.context.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { useFile, FileContext } from 'gitea-react-toolkit';
 import {useEffect} from 'react';
 import { AppContext } from '../App.context';
+import { onOpenValidation } from './onOpenValidations';
 
 const TargetFileContext = React.createContext();
 
@@ -39,81 +40,17 @@ function TargetFileContextProvider({
       onValidated(false);
       //onCriticalErrors(['Validating...']);
     } else if (!validated) {
-      // work with both old and new TN TSV formats
-      if ( state.name.match(/^tn_...\.tsv$|tn_..-...\.tsv$|^twl_...\.tsv$/) ) {
-        const link = state.html_url.replace('/src/', '/blame/');
-        let criticalNotices = [];
-        let tsvFile = state.content;
-        // Split into an array of rows
-        let rows = tsvFile.split('\n');
-        // Is the first row correct (must have the correct headers)
-        //const tsvFormat = rows[0].split('\t').length;
-        let tsvHeader;
-        let tsvFormat;
-        let badTsvHeaderMessage;
-        if ( state.name.match(/tn_..-...\.tsv$/)) {
-          // this is the legacy 9 column format tn tsv filename pattern
-          tsvFormat = 9;
-          tsvHeader = "Book\tChapter\tVerse\tID\tSupportReference\tOrigQuote\tOccurrence\tGLQuote\tOccurrenceNote";
-          badTsvHeaderMessage = `Bad tN (legacy) TSV Header, must have 9 columns`;
-        } else if ( state.name.match(/^tn_...\.tsv$/) ) {
-          // this is the new 6 column tn tsv filename pattern
-          tsvFormat = 7;
-          tsvHeader = "Reference\tID\tTags\tSupportReference\tQuote\tOccurrence\tAnnotation";
-          badTsvHeaderMessage = `Bad tN TSV Header, must have 7 columns`;
-        } else if ( state.name.match(/^twl_...\.tsv$/) ) {
-          // this is the twl tsv filename pattern 
-          tsvFormat = 6;
-          tsvHeader = "Reference\tID\tTags\tOrigWords\tOccurrence\tTWLink";
-          badTsvHeaderMessage = `Bad TWL TSV Header, must have 6 columns`;
-        } else {
-          criticalNotices.push([
-            `${link}#L1`,
-            '1',
-            badTsvHeaderMessage]);
-        }
-        // NOTE: there are cases where invisible characters are at the end 
-        // of the row. This line ensures that the header row only has the
-        // number of characters needed. Only then are they compared.
-        if (tsvHeader !== rows[0]) {
-          criticalNotices.push([
-            `${link}#L1`,
-            '1',
-            `Bad TSV Header, expecting "${tsvHeader.replaceAll('\t', ', ')}"`]);
-        }
+      let criticalNotices = [];
+      criticalNotices = onOpenValidation({
+        filename: state.name, 
+        content: state.content, 
+        url: state.html_url,
+      });
   
-        if (rows.length > 1) {
-          for (let i = 0; i < rows.length; i++) {
-            let line = i + 1;
-            // ignore, skip empty rows
-            if ( rows[i] === undefined || rows[i] === '' ) {
-              continue;
-            }
-            let cols = rows[i].split('\t');
-            if (cols.length < tsvFormat) {
-              criticalNotices.push([
-                `${link}#L${line}`,
-                `${line}`,
-                `Not enough columns, expecting ${tsvFormat}, found ${cols.length}`
-              ])
-            } else if (cols.length > tsvFormat) {
-              criticalNotices.push([
-                `${link}#L${line}`,
-                `${line}`,
-                `Too many columns, expecting ${tsvFormat}, found ${cols.length}`
-              ])
-            }
-          }
-        }
-  
-        if (criticalNotices.length > 0) {
-          onCriticalErrors(criticalNotices);
-        } else {
-          onValidated(true);
-        }
+      if (criticalNotices.length > 0) {
+        onCriticalErrors(criticalNotices);
       } else {
-        console.log("pattern NOT matched for:", state.name);
-        onValidated(true)
+        onValidated(true);
       }
     }
   }, [validated, onValidated, state, onCriticalErrors]);
--- src/core/onOpenValidations.js
+++ src/core/onOpenValidations.js
@@ -16,75 +16,67 @@
 
 const onOpenValidationTn9 = (content, url) => {
   const tsvHeader = "Book\tChapter\tVerse\tID\tSupportReference\tOrigQuote\tOccurrence\tGLQuote\tOccurrenceNote";
-
+  const numColumns = 9;
+  return onOpenValidationTsvGeneric(content, url, tsvHeader, numColumns);
 }
+const onOpenValidationTn7 = (content, url) => {
+  const tsvHeader = "Reference\tID\tTags\tSupportReference\tQuote\tOccurrence\tAnnotation";
+  const numColumns = 7;
+  return onOpenValidationTsvGeneric(content, url, tsvHeader, numColumns);
+}
+const onOpenValidationTwl = (content, url) => {
+  const tsvHeader = "Reference\tID\tTags\tOrigWords\tOccurrence\tTWLink";
+  const numColumns = 6;
+  return onOpenValidationTsvGeneric(content, url, tsvHeader, numColumns);
+}
+const onOpenValidationTsvGeneric = (content, link, tsvHeader, numColumns) => {
+  const rows = content.split('\n');
+  let criticalNotices = [];
 
-export const onOpenValidation = ({filename, content, html_url}) => {
-  if ( filename.match(/^tn_...\.tsv$|tn_..-...\.tsv$|^twl_...\.tsv$/) ) {
-    const link = html_url.replace('/src/', '/blame/');
-    let criticalNotices = [];
-    let tsvFile = content;
-    // Split into an array of rows
-    let rows = tsvFile.split('\n');
-    // Is the first row correct (must have the correct headers)
-    let tsvHeader = "Book\tChapter\tVerse\tID\tSupportReference\tOrigQuote\tOccurrence\tGLQuote\tOccurrenceNote";
-    const tsvHeader7= "Reference\tID\tTags\tSupportReference\tQuote\tOccurrence\tAnnotation";
-    const tsvHeader6= "Reference\tID\tTags\tOrigWords\tOccurrence\tTWLink";
-    const tsvFormat = rows[0].split('\t').length;
-    if ( tsvFormat === 7 ) {
-      tsvHeader = tsvHeader7;
-    } else if ( tsvFormat === 6 ) {
-      tsvHeader = tsvHeader6;
-    } else if ( tsvFormat === 9 ) {
-      // good to go... must be either 7 or 9
-    } else {
-      criticalNotices.push([
-        `${link}#L1`,
-        '1',
-        `Bad TSV Header, must have 7 or 9 columns`]);
-    }
-    // NOTE: there are cases where invisible characters are at the end 
-    // of the row. This line ensures that the header row only has the
-    // number of characters needed. Only then are they compared.
-    rows[0] = rows[0].slice(0,tsvHeader.length);
-    if (tsvHeader !== rows[0]) {
-      criticalNotices.push([
-        `${link}#L1`,
-        '1',
-        `Bad TSV Header, expecting "${tsvHeader.replaceAll('\t', ', ')}"`]);
-    }
-  
-    if (rows.length > 1) {
-      for (let i = 1; i < rows.length; i++) {
-        let line = i + 1;
-        // ignore, skip empty rows
-        if ( rows[i] === undefined || rows[i] === '' ) {
-          continue;
-        }
-        let cols = rows[i].split('\t');
-        if (cols.length < tsvFormat) {
-          criticalNotices.push([
-            `${link}#L${line}`,
-            `${line}`,
-            `Not enough columns, expecting ${tsvFormat}, found ${cols.length}`
-          ])
-        } else if (cols.length > 9) {
-          criticalNotices.push([
-            `${link}#L${line}`,
-            `${line}`,
-            `Too many columns, expecting ${tsvFormat}, found ${cols.length}`
-          ])
-        }
+  if (tsvHeader !== rows[0]) {
+    criticalNotices.push([
+      `${link}#L1`,
+      '1',
+      `Bad TSV Header, expecting "${tsvHeader.replaceAll('\t', ', ')}"`]);
+  }
+
+  if (rows.length > 1) {
+    for (let i = 0; i < rows.length; i++) {
+      let line = i + 1;
+      // ignore, skip empty rows
+      if ( rows[i] === undefined || rows[i] === '' ) {
+        continue;
+      }
+      let cols = rows[i].split('\t');
+      if (cols.length < numColumns) {
+        criticalNotices.push([
+          `${link}#L${line}`,
+          `${line}`,
+          `Not enough columns, expecting ${numColumns}, found ${cols.length}`
+        ])
+      } else if (cols.length > numColumns) {
+        criticalNotices.push([
+          `${link}#L${line}`,
+          `${line}`,
+          `Too many columns, expecting ${numColumns}, found ${cols.length}`
+        ])
       }
     }
-  
-    if (criticalNotices.length > 0) {
-      onCriticalErrors(criticalNotices);
-    } else {
-      onValidated(true);
-    }
-  } else {
-    onValidated(true)
   }
+
+  return criticalNotices;
 }
 
+export const onOpenValidation = ({filename, content, url}) => {
+  const link = url.replace('/src/', '/blame/');
+  let criticalNotices = [];
+
+  if ( filename.match(/^tn_...\.tsv$/) ) {
+    criticalNotices = onOpenValidationTn7(content,link);
+  } else if ( filename.match(/tn_..-...\.tsv$/) ) {
+    criticalNotices = onOpenValidationTn9(content, link);
+  } else if ( filename.match(/^twl_...\.tsv$/) ) {
+    criticalNotices = onOpenValidationTwl(content, link);
+  }
+  return criticalNotices;
+}
