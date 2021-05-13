6b81e5437cdc1a2102792eaf37398b1de7e46e4e
mandolyte <cecil.new@gmail.com>
2021-05-13 12:48:52 -0400

    WIP: adding catalog next

--- src/core/TargetFile.context.js
+++ src/core/TargetFile.context.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { useFile, FileContext } from 'gitea-react-toolkit';
 import { AppContext } from '../App.context';
+import { fetchCatalogContent } from './dcsCatalogNextApis';
 
 const TargetFileContext = React.createContext();
 
@@ -16,6 +17,41 @@ function TargetFileContextProvider({
 
   const { state: sourceFile } = useContext(FileContext);
 
+  console.log("--TargetFileContextProvider--");
+  const appContext = useContext(AppContext);
+  const sourceContext = useContext(FileContext);
+  console.log("app context:", appContext);
+  console.log("source file context:", sourceContext);
+  console.log("target repository:",targetRepository);
+  console.log("filepath:", filepath);
+  //console.log("defaultContent:", defaultContent);
+  let _defaultContent = sourceFile && sourceFile.content;
+  if ( appContext.state.sourceRepository.id === appContext.state.targetRepository.id ) {
+    // Boom! this is an editor not a translator
+    // NOTE: at present the source info is *always* unfoldingword.
+    // So when source and target are the same, then nothing to do
+    // since the default content should come from the master branch
+    // as it always has.
+    console.log("Target is an edit case");
+  } else {
+    // Whoa! this is a translator. In this case, the default content
+    // should come from the uW prod release.
+    // let pull out the info we need
+    const prodTag = appContext.state.sourceRepository.catalog.prod.branch_or_tag_name;
+    console.log("Target is a translate case, use prod release:", prodTag);
+    const getData = (async () => {
+      let data = await fetchCatalogContent(
+        'unfoldingword',
+        appContext.state.sourceRepository.name,
+        prodTag,
+        filepath,
+      )
+      return data;
+    });
+    _defaultContent = getData();
+    console.log("prodTag content:", _defaultContent);
+  }
+
   const {
     state, actions, component, components, config,
   } = useFile({
@@ -24,7 +60,7 @@ function TargetFileContextProvider({
     repository: targetRepository,
     filepath,
     onFilepath: setFilepath,
-    defaultContent: (sourceFile && sourceFile.content),
+    defaultContent: _defaultContent,
     onOpenValidation: onOpenValidation,
   });
 
