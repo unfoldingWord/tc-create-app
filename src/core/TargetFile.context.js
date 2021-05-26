3b62cacd007f966e2c201791a511f8c7c6c8f9a1
mandolyte <cecil.new@gmail.com>
2021-05-26 10:00:03 -0400

    Working version; console logs removed

--- src/core/TargetFile.context.js
+++ src/core/TargetFile.context.js
@@ -1,16 +1,13 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { useFile, FileContext } from 'gitea-react-toolkit';
 import { AppContext } from '../App.context';
-import { fetchCatalogContent } from './dcsCatalogNextApis';
 
 const TargetFileContext = React.createContext();
 
 function TargetFileContextProvider({
   onOpenValidation, children
 }) {
-  const [defaultContent, setDefaultContent] = useState('');
-
   const {
     state: {
       authentication, targetRepository, filepath, setFilepath,
@@ -19,58 +16,32 @@ function TargetFileContextProvider({
 
   const { state: sourceFile } = useContext(FileContext);
 
-  console.log("--TargetFileContextProvider--");
   const appContext = useContext(AppContext);
   const sourceContext = useContext(FileContext);
+
+  /* Some useful debugging logs left here for the future.
+  console.log("--TargetFileContextProvider--");
   console.log("app context:", appContext);
   console.log("source file context:", sourceContext);
   console.log("target repository:",targetRepository);
   console.log("filepath:", filepath);
+  */
 
-  useEffect(() => {
-    let _defaultContent;
-    if ( appContext.state.sourceRepository.id === appContext.state.targetRepository.id ) {
-      // Boom! this is an editor not a translator
-      // NOTE: at present the source info is *always* unfoldingword.
-      // So when source and target are the same, then nothing to do
-      // since the default content should come from the master branch
-      // as it always has.
-      
-      _defaultContent = sourceFile && sourceFile.content;
-      setDefaultContent(_defaultContent);
+  let _defaultContent;
+  if ( appContext.state.sourceRepository.id === appContext.state.targetRepository.id ) {
+    // this is the editor role; they need latest content from master
+    // to be on the source side and as the default content 
+    // if a new file is being edited.
+    _defaultContent = sourceFile && sourceFile.content;
+  } else {
+    // this is the translator role; they require the source side content
+    // to be from the published catalog. For now this is latest prod content.
+    // it also needs to be the default content.
+    _defaultContent = sourceContext.state.publishedContent;
+    // also replease the source content
+    sourceFile.content = _defaultContent;
+  }
 
-      console.log("Target is an edit case");
-    } else {
-      // Whoa! this is a translator. In this case, the default content
-      // should come from the uW prod release.
-      // let pull out the info we need
-      const prodTag = appContext.state.sourceRepository.catalog.prod.branch_or_tag_name;
-      console.log("Target is a translate case, use prod release:", prodTag);
-      const getData = (async () => {
-        let data = await fetchCatalogContent(
-          'unfoldingword',
-          appContext.state.sourceRepository.name,
-          prodTag,
-          filepath,
-        )
-        return data;
-      });
-      getData().then(
-        (results) => {setDefaultContent(results);}
-      );
-    }
-  }, [
-    appContext.state.sourceRepository.id, 
-    appContext.state.targetRepository.id, 
-    appContext.state.sourceRepository.name,
-    filepath,
-    sourceFile,
-    appContext.state.sourceRepository.catalog.prod.branch_or_tag_name,
-    setDefaultContent
-  ]);
-
-
-  console.log("useFile called with default content:", defaultContent);
   const {
     state, actions, component, components, config,
   } = useFile({
@@ -79,7 +50,7 @@ function TargetFileContextProvider({
     repository: targetRepository,
     filepath,
     onFilepath: setFilepath,
-    defaultContent: defaultContent,
+    defaultContent: _defaultContent,
     onOpenValidation: onOpenValidation,
   });
 
