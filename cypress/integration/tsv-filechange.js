8c441e0f809c11b99e24a28e661c37f64903ab55
klappy <christopher@klapp.name>
2022-03-08 15:05:09 -0500

    fix whitespace

--- cypress/integration/tsv-filechange.js
+++ cypress/integration/tsv-filechange.js
@@ -1,47 +1,47 @@
 /// <reference types="cypress" />
 
 describe('File Change', function () {
-    before(() => {
-      cy.visit('/');
-    });
-    it('Select Login', function () {
-      /** Login */
-      cy.get('[data-test=submit-button]').should('have.text', 'Login');
-      cy.get('[data-test=username-input] input').type(Cypress.env('TEST_USERNAME'));
-      cy.get('[data-test=password-input] input').type(Cypress.env('TEST_PASSWORD'));
-      cy.get('[data-test=submit-button]').click();
-    });
-    it('Select Organization', function () {
-      /** Select organization */
-      cy.wait(1000);
-      cy.contains('unfoldingWord').click();
-    });
-    it('Select Resource', function () {
-      /** Select resource */
-      cy.wait(1000);
-      cy.contains('Translation Notes').click();
-    });
-    it('Select Language', function () {
-      /** Select language */
-      cy.get('.language-select-dropdown').click();
-      cy.focused().type('english{enter}');
-    });
-    it('Select File', function () {
-      /** Select file */
-      cy.contains('en_tn_57-TIT.tsv').should('be.be.visible').click();
-    });
-    it('File Change', function () {
-      /**logout process */
-      cy.get('[data-test=drawer-menu-button]').click();
-      cy.contains('en_tn_18-JOB.tsv').click();
-      cy.contains('Introduction to Job', { timeout: 20000 })
+  before(() => {
+    cy.visit('/');
+  });
+  it('Select Login', function () {
+    /** Login */
+    cy.get('[data-test=submit-button]').should('have.text', 'Login');
+    cy.get('[data-test=username-input] input').type(Cypress.env('TEST_USERNAME'));
+    cy.get('[data-test=password-input] input').type(Cypress.env('TEST_PASSWORD'));
+    cy.get('[data-test=submit-button]').click();
+  });
+  it('Select Organization', function () {
+    /** Select organization */
+    cy.wait(1000);
+    cy.contains('unfoldingWord').click();
+  });
+  it('Select Resource', function () {
+    /** Select resource */
+    cy.wait(1000);
+    cy.contains('Translation Notes').click();
+  });
+  it('Select Language', function () {
+    /** Select language */
+    cy.get('.language-select-dropdown').click();
+    cy.focused().type('english{enter}');
+  });
+  it('Select File', function () {
+    /** Select file */
+    cy.contains('en_tn_57-TIT.tsv').should('be.be.visible').click();
+  });
+  it('File Change', function () {
+    /**logout process */
+    cy.get('[data-test=drawer-menu-button]').click();
+    cy.contains('en_tn_18-JOB.tsv').click();
+    cy.contains('Introduction to Job', { timeout: 20000 });
     //   cy.contains('# Introduction to Titus');
     //   cy.get('[data-test=logout-button]').click();
-    });
-    // it('preview markdown', function () {
-    //     /**logout process */
-    //     cy.get('[data-test=preview-icon]').click();
-    //     cy.contains('Introduction to Titus')
-    //   //   cy.get('[data-test=logout-button]').click();
-    //   });
   });
+  // it('preview markdown', function () {
+  //     /**logout process */
+  //     cy.get('[data-test=preview-icon]').click();
+  //     cy.contains('Introduction to Titus')
+  //   //   cy.get('[data-test=logout-button]').click();
+  //   });
+});
--- cypress/integration/tsv-pagination.js
+++ cypress/integration/tsv-pagination.js
@@ -1,44 +1,44 @@
 /// <reference types="cypress" />
 
 describe('pagination', function () {
-    before(() => {
-      cy.visit('/');
-    });
-    it('Select Login', function () {
-      /** Login */
-      cy.get('[data-test=submit-button]').should('have.text', 'Login');
-      cy.get('[data-test=username-input] input').type(Cypress.env('TEST_USERNAME'));
-      cy.get('[data-test=password-input] input').type(Cypress.env('TEST_PASSWORD'));
-      cy.get('[data-test=submit-button]').click();
-    });
-    it('Select Organization', function () {
-      /** Select organization */
-      cy.wait(1000);
-      cy.contains('unfoldingWord').click();
-    });
-    it('Select Resource', function () {
-      /** Select resource */
-      cy.wait(1000);
-      cy.contains('Translation Notes').click();
-    });
-    it('Select Language', function () {
-      /** Select language */
-      cy.get('.language-select-dropdown').click();
-      cy.focused().type('english{enter}');
-    });
-    it('Select File', function () {
-      /** Select file */
-      cy.contains('en_tn_57-TIT.tsv').should('be.be.visible').click();
-    });
-    it('pagination', function () {
-      /**select pagination */
-      cy.get('[data-testid=pagination-rows]').click();
-      cy.contains('50').should('be.be.visible').click();
-    });
-    it('pagination next', function () {
-      /**select next button */
-      cy.get('[data-testid="pagination-next"]').click();
-      cy.get('[data-testid="pagination-next"]').click();
-      cy.contains('As for those believers who are slaves,')
-    });
+  before(() => {
+    cy.visit('/');
   });
+  it('Select Login', function () {
+    /** Login */
+    cy.get('[data-test=submit-button]').should('have.text', 'Login');
+    cy.get('[data-test=username-input] input').type(Cypress.env('TEST_USERNAME'));
+    cy.get('[data-test=password-input] input').type(Cypress.env('TEST_PASSWORD'));
+    cy.get('[data-test=submit-button]').click();
+  });
+  it('Select Organization', function () {
+    /** Select organization */
+    cy.wait(1000);
+    cy.contains('unfoldingWord').click();
+  });
+  it('Select Resource', function () {
+    /** Select resource */
+    cy.wait(1000);
+    cy.contains('Translation Notes').click();
+  });
+  it('Select Language', function () {
+    /** Select language */
+    cy.get('.language-select-dropdown').click();
+    cy.focused().type('english{enter}');
+  });
+  it('Select File', function () {
+    /** Select file */
+    cy.contains('en_tn_57-TIT.tsv').should('be.be.visible').click();
+  });
+  it('pagination', function () {
+    /**select pagination */
+    cy.get('[data-testid=pagination-rows]').click();
+    cy.contains('50').should('be.be.visible').click();
+  });
+  it('pagination next', function () {
+    /**select next button */
+    cy.get('[data-testid="pagination-next"]').click();
+    cy.get('[data-testid="pagination-next"]').click();
+    cy.contains('As for those believers who are slaves,');
+  });
+});
